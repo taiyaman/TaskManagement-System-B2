fcb97e208e91bd59004b57c74ccc383506aff5d7
tai.yamaguchi <taiyaman0306@gmail.com>
2021-01-03 12:26:29 +0000

    Finish edit-attendances

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,19 +71,23 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
     
-    # paramsハッシュからユーザーを取得します。
-    def set_user
-      @user = User.find(params[:id])
+    def basic_info_params
+      params.require(:user).permit(:department, :basic_time, :work_time)
     end
     
+    # paramsハッシュからユーザーを取得します。
+#    def set_user
+#      @user = User.find(params[:id])
+#    end
+    
     # ログイン済みのユーザーか確認します。
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "ログインしてください。"
-        redirect_to login_url
-      end
-    end
+#    def logged_in_user
+#      unless logged_in?
+#        store_location
+#        flash[:danger] = "ログインしてください。"
+#        redirect_to login_url
+#      end
+#    end
     
      # アクセスしたユーザーが現在ログインしているユーザーか確認します。
 #    def correct_user
@@ -92,12 +96,12 @@ class UsersController < ApplicationController
 #    end
     
      # アクセスしたユーザーが現在ログインしているユーザーか確認します。
-    def correct_user
-      redirect_to(root_url) unless current_user?(@user)
-    end
+#   def correct_user
+#      redirect_to(root_url) unless current_user?(@user)
+#    end
     
      # システム管理権限所有かどうか判定します。
-    def admin_user
-      redirect_to root_url unless current_user.admin?
-    end
+#    def admin_user
+#      redirect_to root_url unless current_user.admin?
+#    end
 end
